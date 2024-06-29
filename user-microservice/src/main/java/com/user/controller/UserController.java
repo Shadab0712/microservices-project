40ae00b735785c41195fc8dcad283842d3a0eee8
Shadab0712 <shadab7dec@gmail.com>
2024-06-29 15:52:56 +0530

    added circuit breaker and retry method

--- user-microservice/src/main/java/com/user/controller/UserController.java
+++ user-microservice/src/main/java/com/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.user.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.user.model.User;
 import com.user.service.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
@@ -28,15 +36,34 @@ public class UserController {
 		return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
 	}
 
+	// fallback method for circuit breaker
+	public ResponseEntity<User> ratingsHotelFallback(String id, Exception ex) {
+		logger.info("Fallback method is executed because connecting service is down : ", ex.getMessage());
+		User user = new User();
+		user.setUserId("141234");
+		user.setName("Dummy");
+		user.setEmail("dummy@gmail.com");
+		user.setAbout("This user is created dummy because some service is down");
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
+
+	int retryCount = 1;
+
 	@GetMapping("/{id}")
+//	@CircuitBreaker(name = "ratingsHotelBreaker", fallbackMethod = "ratingsHotelFallback")
+	@Retry(name = "ratingsHotelService", fallbackMethod = "ratingsHotelFallback")
+//	@RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingsHotelFallback")
 	public ResponseEntity<User> getUser(@PathVariable String id) {
-		User userById = userService.getUserById(id);
+		logger.info("Get single user handler method : User Controller ");
+		logger.info("Retry Count : {} ", retryCount);
+		retryCount++;
+		User userById = userService.getUser(id);
 		return new ResponseEntity<>(userById, HttpStatus.OK);
 	}
 
 	@GetMapping("/allUsers")
-	public ResponseEntity<List<User>> getAll(){
-		List<User> allUsers = userService.getAllUsers();
-		return new ResponseEntity<>(allUsers , HttpStatus.OK);
+	public ResponseEntity<List<User>> getAll() {
+		List<User> allUsers = userService.getAllUser();
+		return new ResponseEntity<>(allUsers, HttpStatus.OK);
 	}
 }
